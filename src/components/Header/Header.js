8f499f0d17f809e839077a42b8c870fa72842417
lukaszfranczak <lukaszfranczak@gmail.com>
2018-10-11 21:06:02 +0200

    Header - first scratch

---
+++ src/components/Header/Header.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const header = () => {
+    return (
+        <header>
+            <nav>
+                <ul>
+                    <li><img alt='logo'></img></li>
+                    <li>Wybrane realizacje</li>
+                    <li>Kontakt</li>
+                </ul>
+            </nav>
+        </header>
+    )
+}
+
+export default header;
